0ee96687514a9f78b18deeccc6831ed14732c6c5
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-07-12 23:32:38 +0530

    Spring Boot project initiation

---
+++ src/test/java/com/aniwatch/aniwatch/AniwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniwatch.aniwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
